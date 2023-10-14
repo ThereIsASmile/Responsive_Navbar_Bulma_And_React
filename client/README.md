efe84e099bc575ab5be0d528947836a0d1c746d5
Theresa Miles <ThereIsASmile@gmail.com>
2023-10-14 14:15:31 -0400

    First commit

---
+++ client/README.md
@@ -0,0 +1,29 @@
+# Responsive Hamburger Nav in Bulma 
+with React
+
+
+[Bulma Docs](https://bulma.io/documentation/overview/start/)
+
+[How to use Bulma in React](https://blog.logrocket.com/how-to-use-bulma-css-with-react/)
+
+
+**To get started**
+
+For React App:
+1. npm install bulma
+2. Import bulma into App.js
+    1. import 'bulma/css/bulma.css';
+
+For a regular HTML page:
+1. This goes in the header of index.html
+
+`<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">`
+
+
+
+**Things to know about:**
+
+1. aria stands for Accessible Rich Internet Applications.
+It is a set of attributes used to add HTML elements with improved accessibility for users with disabilities. The aria element is used for screen readers and other assistive technologies. 
+
+
---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+// STYLES 🎨
+import 'bulma/css/bulma.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <div classname="main">
+        <div className="buttons">
+          <button class="button is-primary">Primary</button>
+          <button class="button is-link">Link</button>
+          <button class="button is-info">Info</button>
+          <button class="button is-success">Success</button>
+          <button class="button is-warning">Warning</button>
+          <button class="button is-danger">Danger</button>
+          <button class="button is-black">Black</button>
+          <button class="button is-white">White</button>
+          <button class="button is-dark">Dark</button>
+          <button class="button is-light">Light</button>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default App;
